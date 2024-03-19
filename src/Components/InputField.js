8d8210d763712aeb0c009102bc0bb83336efb445
saikiran003 <kiranarishanpalli@gmail.com>
2024-03-19 22:56:08 +0530

    InputField WIP

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,8 +1,7 @@
-// InputField.js
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InputField = ({ placeholder, value, onChange, type = 'text', disabled = false, required = false, minLength, maxLength, pattern, errorMessage }) => {
+const InputField = ({ placeholder='Enter Text', value, onChange, type = 'text', disabled = false, required = false, minLength, maxLength, pattern, errorMessage, style = {}, errorMessageStyle = {} }) => {
   const handleChange = (e) => {
     if (onChange) {
       onChange(e.target.value);
@@ -22,15 +21,22 @@ const InputField = ({ placeholder, value, onChange, type = 'text', disabled = fa
         maxLength={maxLength}
         pattern={pattern}
         style={{
-          width: '100%',
+          width: '200px',
           padding: '8px',
           fontSize: '16px',
-          border: '1px solid #ccc',
+          border: '1px solid black',
           borderRadius: '4px',
           boxSizing: 'border-box',
+          ...style,
         }}
       />
-      {errorMessage && <p style={{ color: 'red', marginTop: '5px' }}>{errorMessage}</p>}
+      {errorMessage && (
+        <p style={{
+          color: 'red',
+          marginTop: '5px',
+          ...errorMessageStyle, // Merge default error message style with user-defined style
+        }}>{errorMessage}</p>
+      )}
     </div>
   );
 };
@@ -46,6 +52,8 @@ InputField.propTypes = {
   maxLength: PropTypes.number,
   pattern: PropTypes.string,
   errorMessage: PropTypes.string,
+  style: PropTypes.object, // Accept any style object
+  errorMessageStyle: PropTypes.object, // Accept any error message style object
 };
 
 export default InputField;
